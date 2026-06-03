d454a3ed925f8d052bbd6f7d96f7e286f5603e22
Nabila <nabilasayyidatul@apps.ipb.ac.id>
2026-06-03 12:12:49 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Role from "./pages/Role";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import List from "./pages/List";
+import Graph from "./pages/Graph";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* ROLE PAGE */}
+        <Route path="/" element={<Role />} />
+
+        {/* LOGIN USER */}
+        <Route path="/login" element={<Login />} />
+
+        {/* HOME */}
+        <Route path="/home" element={<Home />} />
+
+        {/* LIST */}
+        <Route path="/list" element={<List />} />
+
+        {/* GRAPH */}
+        <Route path="/graph" element={<Graph />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
